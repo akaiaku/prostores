b371445cb14d4e1f98a78655008ccdfe6a6bed53
superman <superman@gmail.com>
2025-09-08 09:21:32 +0700

    .

---
+++ coloring/ios/1.1.2/config.js
@@ -0,0 +1,52 @@
+{
+    "version" : "1",
+    "ads":{
+        "limitAdsTime": 300000,
+        "mute": true,
+        "admob": {
+            "enable": true,
+            "app_id": "ca-app-pub-7223114719058225~6346810051",
+            "banner": "ca-app-pub-7223114719058225/1533467077",
+            "interstitial": "ca-app-pub-7223114719058225/7907303730",
+            "interstitialVideo": "",
+            "rewardVideo": "",
+            "nativeExpress": "",
+            "showVideoAdPercent": 100,
+            "showIntertisialPercent": 20,
+            "nativeAdvance": "",
+            "showBanner": true,
+            "showNativeAdvance": false,
+            "ecmp": "22"
+        },
+        "house_ads": {
+            "config": "https://raw.githubusercontent.com/novelland/image/master/ads.json",
+            "ecmp": "1.5",
+            "enable": false
+        },
+        "unity_ads": {
+            "enable": false,
+            "game_id": "5914803",
+            "banner": "Banner_iOS",
+            "interstitial": "Interstitial_iOS",
+            "ecmp": "9"
+        }
+    },
+    "payments": {
+        "crypto":{
+            "url": ""
+        }
+    },
+    "inapp" : {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+        "share_url": "",
+        "domain": "https://raw.githubusercontent.com/tomatou001/s_te_st/refs/heads/main",
+        "update": {
+          "versionCode": 0,
+          "link": "",
+          "description": "1.0.1",
+          "size": 18000,
+          "packagename": "6742087115",
+          "forceUpdate": true
+        }
+      }
+}
