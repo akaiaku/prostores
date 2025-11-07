19edfbcac037846d72c766bd1628887be11e5e03
superman <superman@gmail.com>
2025-11-07 10:11:27 +0700

    .

---
+++ qplay/android/1.0.0/config.js
@@ -0,0 +1,60 @@
+{
+    "version" : "1",
+    "ads":{
+        "limitAdsTime": 300000,
+        "mute": true,
+        "admob": {
+            "enable": false,
+            "app_id": "ca-app-pub-7223114719058225~3869685171",
+            "banner": "ca-app-pub-7223114719058225/8820304113",
+            "interstitial": "ca-app-pub-7223114719058225/8405371144",
+            "interstitialVideo": "",
+            "rewardVideo": "",
+            "nativeExpress": "",
+            "showVideoAdPercent": 100,
+            "showIntertisialPercent": 20,
+            "nativeAdvance": "ca-app-pub-7223114719058225/7507222440",
+            "showBanner": true,
+            "showNativeAdvance": false,
+            "ecmp": "22"
+        },
+        "house_ads": {
+            "config": "https://raw.githubusercontent.com/novelland/image/master/ads.json",
+            "ecmp": "1.5",
+            "enable": false
+        },
+        "unity_ads": {
+            "enable": false,
+            "game_id": "5914802",
+            "banner": "Banner_Android",
+            "interstitial": "Interstitial_Android",
+            "ecmp": "9"
+        },
+        "iron_src": {
+            "enable": true,
+            "game_id": "242c61dbd",
+            "banner": "mzcdqi1jr9g3y9jh",
+            "interstitial": "dru1cqwu27bb6maz",
+            "native": "sduirbhhpjai448m"
+            "ecmp": "9"
+        }
+    },
+    "payments": {
+        "crypto":{
+            "url": ""
+        }
+    },
+    "inapp" : {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+        "share_url": "",
+        "domain": "https://raw.githubusercontent.com/tomatou001/-dszzffk-3/refs/heads/main",
+        "update": {
+          "versionCode": 0,
+          "link": "",
+          "description": "1.0.1",
+          "size": 18000,
+          "packagename": "6742087115",
+          "forceUpdate": true
+        }
+      }
+}
---
+++ qplay/ios/1.0.0/config.js
@@ -0,0 +1,60 @@
+{
+    "version" : "1",
+    "ads":{
+        "limitAdsTime": 300000,
+        "mute": true,
+        "admob": {
+            "enable": false,
+            "app_id": "ca-app-pub-7223114719058225~1299362152",
+            "banner": "ca-app-pub-7223114719058225/1418232918",
+            "interstitial": "ca-app-pub-7223114719058225/8241719617",
+            "interstitialVideo": "",
+            "rewardVideo": "",
+            "nativeExpress": "",
+            "showVideoAdPercent": 100,
+            "showIntertisialPercent": 20,
+            "nativeAdvance": "ca-app-pub-7223114719058225/1003299943",
+            "showBanner": true,
+            "showNativeAdvance": false,
+            "ecmp": "22"
+        },
+        "house_ads": {
+            "config": "https://raw.githubusercontent.com/novelland/image/master/ads.json",
+            "ecmp": "1.5",
+            "enable": false
+        },
+        "unity_ads": {
+            "enable": false,
+            "game_id": "5914802",
+            "banner": "Banner_Android",
+            "interstitial": "Interstitial_Android",
+            "ecmp": "9"
+        },
+        "iron_src": {
+            "enable": true,
+            "game_id": "242c6ca55",
+            "banner": "ndbd16h19sr2vbk5",
+            "interstitial": "mwljvywq9rbjsffm",
+            "native": "pcl35wryegx1w77n",
+            "ecmp": "9"
+        }
+    },
+    "payments": {
+        "crypto":{
+            "url": ""
+        }
+    },
+    "inapp" : {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+        "share_url": "",
+        "domain": "https://raw.githubusercontent.com/tomatou001/-dszzffk-3/refs/heads/main",
+        "update": {
+          "versionCode": 0,
+          "link": "",
+          "description": "1.0.1",
+          "size": 18000,
+          "packagename": "6742087115",
+          "forceUpdate": true
+        }
+      }
+}
