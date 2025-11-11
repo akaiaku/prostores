e35e2839bdc453917ed38dfc6900dd7db3fbc4e6
superman <superman@gmail.com>
2025-11-11 20:04:33 +0700

    .

---
+++ qplay/android/1.0.1/config.js
@@ -0,0 +1,62 @@
+{
+    "version" : "1",
+    "ads":{
+        "limitAdsTime": 300000,
+        "mute": true,
+        "admob": {
+            "enable": true,
+            "app_id": "ca-app-pub-7223114719058225~3869685171",
+            "banner": "ca-app-pub-7223114719058225/8820304113",
+            "interstitial": "ca-app-pub-7223114719058225/8405371144",
+            "interstitialVideo": "",
+            "rewardVideo": "",
+            "nativeExpress": "",
+            "showVideoAdPercent": 100,
+            "showIntertisialPercent": 20,
+            "nativeAdvance": "ca-app-pub-7223114719058225/7507222440",
+            "showBanner": true,
+            "showNativeAdvance": false,
+            "ecmp": "22"
+        },
+        "house_ads": {
+            "config": "https://raw.githubusercontent.com/novelland/image/master/ads.json",
+            "ecmp": "1.5",
+            "enable": false
+        },
+        "unity_ads": {
+            "enable": true,
+            "game_id": "5980944",
+            "banner": "Banner_Android",
+            "interstitial": "Interstitial_Android",
+            "ecmp": "9"
+        },
+        "vungle": {
+            "ecmp": "0.95",
+            "placement_interstitial": "PLACEMENT_INTERSTITIAL-4255975",
+            "placement_ref_banner_id": "PLACEMENT_REF_BANNER_ID-8647405",
+            "placement_ref_native_id": "PLACEMENT_REF_NATIVE_ID-4673008",
+            "placement_ref_id": "",
+            "report_id": "690defaeb08c2c75c9e1c709",
+            "app_id": "690defaeb08c2c75c9e1c709",
+            "enable": true
+        }
+    },
+    "payments": {
+        "crypto":{
+            "url": ""
+        }
+    },
+    "inapp" : {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+        "share_url": "",
+        "domain": "https://raw.githubusercontent.com/tomatou001/-dszzffk-3/refs/heads/main",
+        "update": {
+          "versionCode": 0,
+          "link": "",
+          "description": "1.0.1",
+          "size": 18000,
+          "packagename": "6742087115",
+          "forceUpdate": true
+        }
+      }
+}
---
+++ qplay/ios/1.0.1/config.js
@@ -0,0 +1,62 @@
+{
+    "version" : "1",
+    "ads":{
+        "limitAdsTime": 300000,
+        "mute": true,
+        "admob": {
+            "enable": true,
+            "app_id": "ca-app-pub-7223114719058225~1299362152",
+            "banner": "ca-app-pub-7223114719058225/1418232918",
+            "interstitial": "ca-app-pub-7223114719058225/8241719617",
+            "interstitialVideo": "",
+            "rewardVideo": "",
+            "nativeExpress": "",
+            "showVideoAdPercent": 100,
+            "showIntertisialPercent": 20,
+            "nativeAdvance": "ca-app-pub-7223114719058225/1003299943",
+            "showBanner": true,
+            "showNativeAdvance": false,
+            "ecmp": "22"
+        },
+        "house_ads": {
+            "config": "https://raw.githubusercontent.com/novelland/image/master/ads.json",
+            "ecmp": "1.5",
+            "enable": false
+        },
+        "unity_ads": {
+            "enable": false,
+            "game_id": "5914802",
+            "banner": "Banner_Android",
+            "interstitial": "Interstitial_Android",
+            "ecmp": "9"
+        },
+        "vungle": {
+            "ecmp": "0.95",
+            "placement_interstitial": "DEFAULT51196",
+            "placement_ref_banner_id": "BANNER_250-1883709",
+            "placement_ref_native_id": "NATIVE250-5456932",
+            "placement_ref_id": "",
+            "report_id": "598c150d9fa26d1d690030b9",
+            "app_id": "test00000000001",
+            "enable": false
+        }
+    },
+    "payments": {
+        "crypto":{
+            "url": ""
+        }
+    },
+    "inapp" : {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+        "share_url": "",
+        "domain": "https://raw.githubusercontent.com/tomatou001/-dszzffk-3/refs/heads/main",
+        "update": {
+          "versionCode": 0,
+          "link": "",
+          "description": "1.0.1",
+          "size": 18000,
+          "packagename": "6742087115",
+          "forceUpdate": true
+        }
+      }
+}
